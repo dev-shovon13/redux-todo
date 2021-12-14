8e53b8d3c310bd39963d4cf0030aa56a3d8291c8
mohammed asif <mohammed.asif@techhack.ca>
2021-12-14 11:28:24 +0600

    delete added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,67 @@
 import "./App.css";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState();
+
+  const handleClick = () =>
+    dispatch({
+      type: "ADD_TODO",
+      payload: {
+        label: newTodo,
+        id: Math.ceil(Math.random() * 100),
+      },
+    });
+
   const Todos = () => {
-    const todos = useSelector((state) => state.todos);
+    const todos = useSelector((state) => state.todo.todos);
+
+    const handleDelete = (id) => {
+      dispatch({
+        type: "DELETE_TODO",
+        payload: id,
+      });
+    };
+
     if (!todos || !todos.length) {
-      return <h4>No Todos</h4>;
+      return <h4 className="text-center text-success">No Todos</h4>;
     }
     return (
       <>
-        <ul>
+        <div className="">
           {todos.map((todo) => (
-            <li>{todo.label}</li>
+            <div
+              key={todo.id}
+              className="d-flex justify-content-between bg-light p-1 mb-2 rounded"
+            >
+              <h5 className="text-capitalize">{todo.label}</h5>
+              <div
+                className="btn btn-sm btn-danger"
+                onClick={() => handleDelete(todo.id)}
+              >
+                Delete
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       </>
     );
   };
-  const TodoInput = () => {
-    const [newTodo, setNewTodo] = useState();
-    const dispatch = useDispatch();
-    const handleClick = () =>
-      dispatch({
-        type: "ADD_TODO",
-        payload: {
-          label: newTodo,
-          id: Math.ceil(Math.random() * 100),
-        },
-      });
 
-    return (
-      <>
+  return (
+    <div className="w-25 mx-auto bg p-5">
+      <div className="d-flex mb-4">
         <input
           type="text"
-          className="form-control w-25 mx-auto mt-5"
+          className="form-control w-75"
           onChange={(e) => setNewTodo(e.target.value)}
         />
-        <button onClick={handleClick} className="btn btn-success my-3">
+        <button onClick={handleClick} className="btn btn-success w-25">
           Add Todo
         </button>
-      </>
-    );
-  };
-  return (
-    <div className="App">
-      <TodoInput />
+      </div>{" "}
       <Todos />
     </div>
   );
